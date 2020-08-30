b9e06324e09bf9590fb10cb269b8fa47a72d6db2
yoshi314 <yoshi314@gmail.com>
2020-08-30 20:22:28 +0200

    update newsboat config for some sane defaults

--- .i3/fileage.sh
+++ .i3/fileage.sh
@@ -3,11 +3,18 @@
 file="/home/yoshi/last.txt"
 
 age=$(stat -c %Z $file)
-
 now=$(date +%s)
-
 howlong=$(echo "scale=4; ($now-$age)/86400" | bc -l)
-
 howlongyear=$(echo "scale=4; ($now-$age)/86400/365*100" | bc -l)
 
-echo "PowerLevel $howlong ($howlongyear % of year)"
+msg="P $howlong ($howlongyear %)"
+
+file2="/home/yoshi/lastf.txt"
+age=$(stat -c %Z $file2)
+now=$(date +%s)
+howlong=$(echo "scale=4; ($now-$age)/86400" | bc -l)
+howlongyear=$(echo "scale=4; ($now-$age)/86400/365*100" | bc -l)
+
+
+
+echo "$msg,F $howlong ($howlongyear %)"
